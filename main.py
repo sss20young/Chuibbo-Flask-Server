bc7712856d495457097fe3ec02bee89bdc957058
sss20young <ksyt98@gmail.com>
2022-03-23 02:48:02 +0900

    refactor: change according to rest api rule

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 """
 
 from flask import Flask, jsonify, request, Response
+from flask_restx import Api, Resource
 
 import os
 import argparse
@@ -31,6 +32,14 @@ from PIL import Image
 import traceback
 
 app = Flask(__name__)
+
+""" swagger-ui """
+api = Api(app, version='0.0.1', title='[ Chuibbo-Flask ]', description='Chuibbo API 명세서', doc="/swagger-ui")
+resume_photo_api = api.namespace('api/resume-photos', description='Resume Photo Controller')
+upload_api = api.namespace('api/upload', description='Upload Controller')
+parameter_api = api.namespace('api/parameter', description='Parameter Controller')
+strong_api = api.namespace('api/strong', description='Strong Controller')
+makeup_api = api.namespace('api/makeup', description='Makeup Controller')
 
 """ MAKE UP """
 def data(size, r, g, b, index):
@@ -80,131 +89,143 @@ def main(args):
 hair_dict = {'long' : 1, 'mid' : 2, 'short' : 3}
 
 """ RESUME PHOTO API """
-@app.route('/api/resume_photo/', methods=['POST'])
-def resume_photo():
-    # STEP 1: 요청한 사진과 관련 정보 받고, assets/representative/resume/src 폴더에 사진 저장
-    photo = request.files['photo']
-    id = request.form['id']
-    sex = request.form['sex']
-    face_shape = request.form['face_shape']
-    hairstyle = request.form['hairstyle'] # 취업 사진 헤어스타일
-    prev_hairstyle = 'mid'
-    # prev_hairstyle = request.form['prev_hairstyle'] # TODO: 기존의 본인 헤어스타일
-    suit = request.form['suit']
+@resume_photo_api.route('')
+class ResumePhoto(Resource):
+    def post(self):
+        """이력서 사진 생성"""
+        # STEP 1: 요청한 사진과 관련 정보 받고, assets/representative/resume/src 폴더에 사진 저장
+        photo = request.files['photo']
+        id = request.form['id']
+        sex = request.form['sex']
+        face_shape = request.form['face_shape']
+        hairstyle = request.form['hairstyle'] # 취업 사진 헤어스타일
+        prev_hairstyle = 'mid'
+        # prev_hairstyle = request.form['prev_hairstyle'] # TODO: 기존의 본인 헤어스타일
+        suit = request.form['suit']
 
-    arguments.selected_hairstyle = hairstyle
-    src_dir_path = f'./assets/representative/resume/src/{prev_hairstyle}/'
-    date = '{:04d}'.format(datetime.today().year) + '{:02d}'.format(datetime.today().month) + '{:02d}'.format(datetime.today().day) + '{:02d}'.format(datetime.today().hour) + '{:02d}'.format(datetime.today().minute)
-    src_image = f'{id}_{date}.jpg' # 사진이름 동적으로 생성 ex. 아이디+날짜시간
-    src_image_path = src_dir_path + src_image
-    with open(src_image_path, 'wb') as f:
-        f.write(photo.read())
+        arguments.selected_hairstyle = hairstyle
+        src_dir_path = f'./assets/representative/resume/src/{prev_hairstyle}/'
+        date = '{:04d}'.format(datetime.today().year) + '{:02d}'.format(datetime.today().month) + '{:02d}'.format(datetime.today().day) + '{:02d}'.format(datetime.today().hour) + '{:02d}'.format(datetime.today().minute)
+        src_image = f'{id}_{date}.jpg' # 사진이름 동적으로 생성 ex. 아이디+날짜시간
+        src_image_path = src_dir_path + src_image
+        with open(src_image_path, 'wb') as f:
+            f.write(photo.read())
 
-    # 선언을 위한 할당(초기화)
-    result_image_path = './expr/results/resume/example.jpg'
-    result_image_png = './expr/results/resume/example.png'
+        # 선언을 위한 할당(초기화)
+        result_image_path = './expr/results/resume/example.jpg'
+        result_image_png = './expr/results/resume/example.png'
 
-    print("FINISH STEP1")
+        print("FINISH STEP1")
 
-    try:
-        # STEP 2: 사람이 감지되는지 확인
-        number_of_face_detection = detect_faces(src_image_path)
-        if number_of_face_detection >= 2: # 2인 이상 감지되었을 때
-            print(number_of_face_detection)
-            return jsonify({ 'code': 2, 'message': '2인 이상 감지되었습니다.'}), 200
-        elif number_of_face_detection == 0: # 사람이 아무도 감지되지 않았을 때
-            print(number_of_face_detection)
-            return jsonify({ 'code': 3, 'message': '얼굴 인식에 실패하였습니다.'}), 200
+        try:
+            # STEP 2: 사람이 감지되는지 확인
+            number_of_face_detection = detect_faces(src_image_path)
+            if number_of_face_detection >= 2: # 2인 이상 감지되었을 때
+                print(number_of_face_detection)
+                delete_image(src_image_path)
+                return jsonify({ 'code': 2, 'message': '2인 이상 감지되었습니다.'}), 200
+            elif number_of_face_detection == 0: # 사람이 아무도 감지되지 않았을 때
+                print(number_of_face_detection)
+                delete_image(src_image_path)
+                return jsonify({ 'code': 3, 'message': '얼굴 인식에 실패하였습니다.'}), 200
 
-        print("FINISH STEP2")
+            print("FINISH STEP2")
 
-        # STEP 3: 전처리(얼굴 가운데로 맞추는) 실행 후 origin image에 덮어쓰기
-        preprocessing_crop(src_image_path) # TODO: 도메인별(여-남, 헤어스타일 등)에 따라 다른 값 주기
+            # STEP 3: 전처리(얼굴 가운데로 맞추는) 실행 후 origin image에 덮어쓰기
+            preprocessing_crop(src_image_path) # TODO: 도메인별(여-남, 헤어스타일 등)에 따라 다른 값 주기
 
-        print("FINISH STEP3")
+            print("FINISH STEP3")
 
-        # STEP 4: 모델을 통해 resume photo 생성
-        # TODO: 사진 저장 시, 결과 사진 한 장만 저장
-        arguments.result_image_name = src_image # 저장될 이미지 파일 이름 지정
-        print("----- Start creating resume photo!! -----")
-        main(arguments)
+            # STEP 4: 모델을 통해 resume photo 생성
+            # TODO: 사진 저장 시, 결과 사진 한 장만 저장
+            arguments.result_image_name = src_image # 저장될 이미지 파일 이름 지정
+            print("----- Start creating resume photo!! -----")
+            main(arguments)
 
-        print("FINISH STEP4")
+            print("FINISH STEP4")
 
-        image_title, image_ext = os.path.splitext(src_image)
-        # '_' + str(hair_dict[hairstyle]) +
-        result_dir_path = './expr/results/resume'
-        result_image_path = f'{result_dir_path}/{src_image}' # TODO: 파일 이름 랜덤으로 secure하도록
-        result_image_jpg = Image.open(result_image_path)
-        result_image_png = f'{result_dir_path}/{image_title}.png'
-        result_image_jpg.save(result_image_png) # png로 변환
-        encoded_img = transform_encoded_image(result_image_png)
+            image_title, image_ext = os.path.splitext(src_image)
+            # '_' + str(hair_dict[hairstyle]) +
+            result_dir_path = './expr/results/resume'
+            result_image_path = f'{result_dir_path}/{src_image}' # TODO: 파일 이름 랜덤으로 secure하도록
+            result_image_jpg = Image.open(result_image_path)
+            result_image_png = f'{result_dir_path}/{image_title}.png'
+            result_image_jpg.save(result_image_png) # png로 변환
+            encoded_img = transform_encoded_image(result_image_png)
 
-    except:
-        delete_image(src_image_path)
-        delete_image(result_image_path)
-        delete_image(result_image_png)
+        except:
+            delete_image(src_image_path)
+            delete_image(result_image_path)
+            delete_image(result_image_png)
 
-        print(traceback.format_exc())
+            print(traceback.format_exc())
 
-        return jsonify({ 'code': 4, 'message': '사진 합성 중 예기치 못한 오류가 발생하였습니다.'}), 200
+            return jsonify({ 'code': 4, 'message': '사진 합성 중 예기치 못한 오류가 발생하였습니다.'}), 200
 
-    else:
-        # STEP 5: assets/representative/resume/src 폴더에 저장된 사진 삭제
-        delete_image(src_image_path)
-        delete_image(result_image_path)
-        delete_image(result_image_png)
+        else:
+            # STEP 5: assets/representative/resume/src 폴더에 저장된 사진 삭제
+            delete_image(src_image_path)
+            delete_image(result_image_path)
+            delete_image(result_image_png)
 
-        print("FINISH STEP5")
+            print("FINISH STEP5")
 
-        return json.dumps({ 'code': 1, 'message': '', 'data': encoded_img.decode('ascii') }), 200
+            return json.dumps({ 'code': 1, 'message': '', 'data': encoded_img.decode('ascii') }), 200
 
 
 """ MAKEUP API """
-@app.route('/api/upload', methods=['POST'])
-def upload():
-    # 이미지 받아오기
-    img = cv.imdecode(np.fromstring(request.files['image'].read(), np.uint8), cv.IMREAD_UNCHANGED)
-    w, h = img.shape[1], img.shape[0]
-    if w-h > 1000:
-        img = cv.rotate(img, cv.ROTATE_90_CLOCKWISE)
-    # 서버 내 이미지 저장
-    path_file = ('static/Input.jpg')
-    cv.imwrite(path_file, img)
-    img_processed = json.dumps(path_file)
-    # json string으로 돌려받기
-    return Response(response=img_processed, status=200, mimetype="application/json")
+@upload_api.route('')
+class Upload(Resource):
+    def post(self):
+        """메이크업할 사진 업로드"""
+        # 이미지 받아오기
+        img = cv.imdecode(np.fromstring(request.files['image'].read(), np.uint8), cv.IMREAD_UNCHANGED)
+        w, h = img.shape[1], img.shape[0]
+        if w-h > 1000:
+            img = cv.rotate(img, cv.ROTATE_90_CLOCKWISE)
+        # 서버 내 이미지 저장
+        path_file = ('static/Input.jpg')
+        cv.imwrite(path_file, img)
+        img_processed = json.dumps(path_file)
+        # json string으로 돌려받기
+        return Response(response=img_processed, status=200, mimetype="application/json")
 
-@app.route('/api/parameter', methods=['POST'])
-def parameter():
-    #print(request.is_json)
-    params = request.get_json()
-    print(params)
-    size = params.get('size')
-    r = params.get('rColor')
-    g = params.get('gColor')
-    b = params.get('bColor')
-    index = params.get('index')
-    data(size, r, g, b, index)
-    return Response()
+@parameter_api.route('')
+class Parameter(Resource):
+    def post():
+        """메이크업 RGB 색상값 전달"""
+        #print(request.is_json)
+        params = request.get_json()
+        print(params)
+        size = params.get('size')
+        r = params.get('rColor')
+        g = params.get('gColor')
+        b = params.get('bColor')
+        index = params.get('index')
+        data(size, r, g, b, index)
+        return Response()
 
-@app.route('/api/strong', methods=['POST'])
-def strong():
-    #print(request.is_json)
-    params = request.get_json()
-    #print(params/200)
-    dataStrong(params/200)
-    return Response()
+@strong_api.route('')
+class Strong(Resource):
+    def post():
+        """메이크업 강도 전달"""
+        #print(request.is_json)
+        params = request.get_json()
+        #print(params/200)
+        dataStrong(params/200)
+        return Response()
 
-@app.route('/api/makeup', methods=['POST'])
-def makeUpFace():
-    params = request.get_json()
-    print("메이크업 시작......")
-    print(GR, GG, GB, GS, ID, GT)
-    make = makeUp()
-    make.readImg()  # 이미지 초기화
-    make.makeUpFeatures(r=GR, g=GG, b=GB, size=(GS, GS), index=ID, strong=GT)
-    return Response(response=params)
+@makeup_api.route('')
+class Makeup(Resource):
+    def post():
+        """메이크업 실행"""
+        params = request.get_json()
+        print("메이크업 시작......")
+        print(GR, GG, GB, GS, ID, GT)
+        make = makeUp()
+        make.readImg()  # 이미지 초기화
+        make.makeUpFeatures(r=GR, g=GG, b=GB, size=(GS, GS), index=ID, strong=GT)
+        return Response(response=params)
 
 
 if __name__ == '__main__':
